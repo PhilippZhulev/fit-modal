e52cde670d5de1eeff04fe85651e3f4e2cc87a5d
PhilippZhulev <Filippja@gmail.com>
2017-10-17 14:46:10 +0300

    version 1.14.2

--- dist_src/jquery.fit_modal.js
+++ dist_src/jquery.fit_modal.js
@@ -18,7 +18,7 @@
             modal_title             :  'Modal window', //modal title text
             modal_content_block     :  null, //modal content block
             frame_animation_speed   :  200, //frame animation time
-            win_animation_speed     :  400, //window animation time
+            win_animation_speed     :  500, //window animation time
             window_animation_type   : 'fade_in_top', //window animation type
             modal_close             : '.modal__window__close', //class buttons close
             fast_create             : true, //generate modal window elements
